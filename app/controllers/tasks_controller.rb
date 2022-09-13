5004f3103172de712867088d20d9e37f8c105578
Jelani Woods <jelani@firstdraft.com>
2022-09-13 10:15:38 -0500

    Allow Task toggle completion

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,11 @@ class TasksController < ApplicationController
     end
   end
 
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: params[:completed])
+    render json: { message: "Success" }
+  end
 
   private
   def task_params
